76d37665b48d38608a308f11e53af2bce7a8e52d
JTLiner925 <[jtliner925@gmail.com]>
2019-12-12 11:35:39 -0600

    step 2

---
+++ src/api.js
@@ -0,0 +1,19 @@
+
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/jtscott';
+
+
+
+function getItems(){
+    return Promise.resolve('A successful response!');
+    
+};
+
+
+
+
+
+
+
+export default {
+    getItems
+};
